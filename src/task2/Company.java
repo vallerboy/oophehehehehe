00ddb5a483f948fa566d85973b18522e3b069542
Oskar <oskarbank@gmail.com>
2017-10-20 13:18:08 +0200

    asd

---
+++ src/task2/Company.java
@@ -0,0 +1,46 @@
+package task2;
+
+import task1.Player;
+
+import java.util.stream.Stream;
+
+public class Company {
+    private Employee[] workers;
+    private Employee[] managers;
+
+    public Company(int workersLength, int managersLength) {
+        workers = new Employee[workersLength];
+        managers = new Employee[managersLength];
+    }
+
+    public boolean addWorker(Employee worker){
+        for (int i = 0; i < workers.length; i++) {
+            if(workers[i] == null){
+                workers[i] = worker;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addManager(Employee manager){
+        for (int i = 0; i < managers.length; i++) {
+            if(managers[i] == null){
+                managers[i] = manager;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void show() {
+        for (Employee manager : managers) {
+            System.out.println(manager.toString());
+        }
+        for (Employee worker : workers) {
+            System.out.println(worker.toString());
+        }
+        
+
+    }
+}
---
+++ src/task2/Employee.java
@@ -0,0 +1,59 @@
+package task2;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+    private int seniority;
+
+    public Employee(String name, String surname, int yearOfBirth, int seniority) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.seniority = seniority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getSeniority() {
+        return seniority;
+    }
+
+    public void setSeniority(int seniority) {
+        this.seniority = seniority;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", seniority=" + seniority +
+                '}';
+    }
+}
+
+
