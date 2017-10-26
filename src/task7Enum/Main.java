2050bf2f4d5420bacc433df67714377e43cc857c
Oskar <oskarbank@gmail.com>
2017-10-26 17:53:05 +0200

    wda

--- src/task7Enum/Main.java
+++ src/task7Enum/Main.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Weather today = new Weather(LocalDate.now(), WeatherInfo.CLOUDY);
-        Weather yesterday = new Weather(LocalDate.now().minusDays(1), WeatherInfo.RAINY);
+        Weather yesterday = new Weather(LocalDate.of(2015, 5, 8), WeatherInfo.RAINY);
 
         System.out.println(today.getWeatherInfo().getDescription());
         System.out.println(yesterday.getWeatherInfo().getAvgTemp());
