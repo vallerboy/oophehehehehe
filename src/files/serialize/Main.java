24b3cb45b59c1866c9f2818d5c494eb4575fd580
Oskar <oskarbank@gmail.com>
2017-10-27 17:44:10 +0200

    asdsd

---
+++ src/files/serialize/Main.java
@@ -0,0 +1,28 @@
+package files.serialize;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("C:\\asd\\cos.bin");
+        Person person = new Person("Ktosiek", "Ktosiowaty", 14);
+
+
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        try {
+            ObjectOutputStream objectOutputStream =
+                    new ObjectOutputStream(new FileOutputStream(file));
+            objectOutputStream.writeObject(person);
+            objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
