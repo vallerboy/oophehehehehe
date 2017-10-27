e4f7e0f4ffd1146cccad2c6e4747381a99cb44ab
Oskar <oskarbank@gmail.com>
2017-10-27 13:39:51 +0200

    asd

--- src/files/Main.java
+++ src/files/Main.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("C:\\asd\\obrazek.png");
         File file1 = new File("C:\\asd\\mojskopiowanyplik.png");
 
-        InputStreamReader inputStreamReader = null;
         List<Integer> byteList = new ArrayList<>();
+
         try {
-            inputStreamReader =
-                    new InputStreamReader(new FileInputStream(file));
+            
+            FileInputStream inputStreamReader = new FileInputStream(file);
             int i;
             while ((i = inputStreamReader.read()) != -1){
                 byteList.add(i);
@@ -22,9 +22,6 @@ public class Main {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if(inputStreamReader != null)
-                inputStreamReader.close();
         }
 
 
@@ -32,8 +29,7 @@ public class Main {
             if(!file1.exists()){
                 file1.createNewFile();
             }
-            FileOutputStream stream = new FileOutputStream(file1, false);
-
+            FileOutputStream stream = new FileOutputStream(file1);
             for (Integer integer : byteList) {
                 stream.write(integer);
             }
